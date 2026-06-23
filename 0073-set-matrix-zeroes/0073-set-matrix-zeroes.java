c361a78653b4f33eeefac870b638c68a7ffbd73c
Parth <knightsaber2246@gmail.com>
2026-06-23 22:39:21 +0530

    Time: 1 ms (98.45%), Space: 47.5 MB (80.04%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,28 +1,24 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Deque <Integer> stack = new ArrayDeque<>();
-        for(int i = 0;i<matrix.length;i++){
-            for(int j = 0; j<matrix[0].length;j++){
-                if(matrix[i][j]==0){
-                    stack.push(i);
-                    stack.push(j);
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
-        while(!stack.isEmpty()){
-            int col = stack.pop();
-            int row = stack.pop();
-            zero(matrix,row,col);
-        }
-
-        
-    }
-    public void zero (int [][] matrix , int row,int col){
-        for(int i = 0;i<matrix.length;i++){
-            matrix [i][col]=0;
-        }
-        for(int j = 0; j<matrix[0].length;j++){
-            matrix[row][j]=0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
         }
     }
 }
