0d992fd9b45724545a866a622521b9d042a55b46
Parth <knightsaber2246@gmail.com>
2026-07-24 22:04:42 +0530

    Time: 2 ms (99.95%), Space: 52.6 MB (40.65%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,28 +1,51 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
 
-        Arrays.sort(nums);
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
 
-        int prev = nums[0];
-        int count = 1;
+        // First Pass: Find possible candidates
+        for (int num : nums) {
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == prev) {
-                count++;
-            } else {
-                if (count > nums.length / 3) {
-                    list.add(prev);
-                }
-                prev = nums[i];
-                count = 1;
+            if (num == candidate1) {
+                count1++;
+            } 
+            else if (num == candidate2) {
+                count2++;
+            } 
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } 
+            else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } 
+            else {
+                count1--;
+                count2--;
             }
         }
 
-        if (count > nums.length / 3) {
-            list.add(prev);
+        // Second Pass: Verify candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1)
+                count1++;
+            else if (num == candidate2)
+                count2++;
         }
 
-        return list;
+        List<Integer> ans = new ArrayList<>();
+
+        if (count1 > nums.length / 3)
+            ans.add(candidate1);
+
+        if (count2 > nums.length / 3)
+            ans.add(candidate2);
+
+        return ans;
     }
 }
