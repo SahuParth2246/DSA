786ee52b56f45a526f3f354f63b75251e32bbafb
Parth <knightsaber2246@gmail.com>
2026-08-03 14:22:32 +0530

    Time: 29 ms (33.01%), Space: 86.7 MB (12.23%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,29 +1,30 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int low = 0;
-        int high = nums.length-1;
-        quicksort(nums,low,high);
+        int start = 0;
+        int end = nums.length-1;
+        merge(nums,start,end);
         return nums;
     }
-    public void quicksort(int [] nums,int low,int high){
-        if(low>=high)return;
-        int pivot = nums[low+(high-low)/2];
-        int end = high;
-        int start = low;
-        while(start<=end){
-            while(nums[start]<pivot)start++;
-            while(nums[end]>pivot)end--;
-            if(start <=end){
-                int temp = nums[end];
-                nums[end]= nums[start];
-                nums[start]=temp;
-                start++;
-                end--;
-
-            }
-
+    public void merge(int [] nums , int start , int end){
+        if(end-start == 0)return;
+        int mid = start + (end-start)/2;
+        merge(nums,start,mid);
+        merge(nums,mid+1,end);
+        merged(nums,start,mid ,end);
+    }
+    public void merged(int [] nums, int start , int mid ,int end){
+        int [] mix = new int [end-start+1];
+        int i = start,j = mid+1,index=0;
+        while(i<=mid&&j<=end){
+            if(nums[i]<nums[j])mix[index++]=nums[i++];
+            else mix[index++]=nums[j++];
         }
-        quicksort(nums,low,end);
-        quicksort(nums,start,high);
+        while(i <=mid)mix[index++]=nums[i++];
+        while(j<=end)mix[index++]=nums[j++];
+
+        for(int k = 0;k<mix.length;k++){
+            nums[start+k]=mix[k];
+        }
+        
     }
 }
