3d918a8293765d50cf0ff7dd2ae52cb3c1ac1071
Parth <knightsaber2246@gmail.com>
2026-08-08 11:58:19 +0530

    Time: 4 ms (82.85%), Space: 46.6 MB (16.6%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(t.length()!=s.length())return false;
-        HashMap <Character,Integer> map = new HashMap <>();
-        for(int i = 0;i<s.length();i++){
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-        }
-        for(int i = 0;i<s.length();i++){
-            if(!map.containsKey(t.charAt(i)) || map.get(t.charAt(i))==0) return false;
-            map.put(t.charAt(i),map.get(t.charAt(i))-1);
-        }
-        return true;
+        if (s.length() != t.length()) return false;
         
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        
+        return Arrays.equals(sArr, tArr);
     }
 }
