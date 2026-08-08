3f2134ff0f9cbc9655c922a5f23447442c5cea66
Parth <knightsaber2246@gmail.com>
2026-08-08 09:41:48 +0530

    Time: 1 ms (87.1%), Space: 45.3 MB (79.24%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,31 +1,26 @@
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
-    boolean[] used;
+    boolean [] used ;
 
-    public List<List<Integer>> permute(int[] nums) {
-        used = new boolean[nums.length];
-        backtrack(nums, new ArrayList<>());
+    public List<List<Integer>> permute(int[]nums){
+        used = new boolean [nums.length];
+        backtrack(nums,new ArrayList<>());
         return result;
-    }
 
-    void backtrack(int[] nums, List<Integer> current) {
-        // BASE CASE
-        if (current.size() == nums.length) {
-            result.add(new ArrayList<>(current));  // copy, not reference!
+    }
+    public void backtrack (int [] nums ,List <Integer> current){
+        if(current.size()==nums.length){
+            result.add(new ArrayList<>(current));
             return;
         }
 
-        // LOOP from 0 every time
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i]) continue;          // skip already used
-
-            used[i] = true;                 // choose
+        for(int i = 0;i<nums.length;i++){
+            if(used[i])continue;
+            used[i]=true;
             current.add(nums[i]);
-
-            backtrack(nums, current);       // explore
-
-            used[i] = false;                // unchoose (backtrack)
-            current.remove(current.size() - 1);
+            backtrack(nums,current);
+            used[i] = false;
+            current.remove(current.size()-1);
         }
     }
 }
